aca3fbf13c384baa6f700809f6c8cb3ebd88cc36
Chris <cdunham1989@gmail.com>
2017-10-11 16:46:52 +0100

    web helper refactoring

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,9 +1,6 @@
 feature 'Hit points' do
   scenario 'Show the players hit points' do
-    visit('/')
-    fill_in :player_1_name, with: 'Rory'
-    fill_in :player_2_name, with: 'Chris'
-    click_button ('Submit')
+    sign_in_and_play
     expect(page).to have_content 'Chris: 60HP'
   end
 end
--- spec/features/names_spec.rb
+++ spec/features/names_spec.rb
@@ -1,9 +1,6 @@
 feature 'Entering Players' do
   scenario 'Can enter the name of two players' do
-    visit('/')
-    fill_in :player_1_name, with: 'Rory'
-    fill_in :player_2_name, with: 'Chris'
-    click_button ('Submit')
+    sign_in_and_play
     expect(page).to have_content 'Rory vs. Chris'
   end
 end
