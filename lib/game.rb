677492a0ff5d61b7068c5c3ab78aebf6d6553629
Jon Sanders <jonsanders101@aol.com>
2017-10-12 16:56:07 +0100

    Added shift as leader for tomorrow

--- lib/game.rb
+++ lib/game.rb
@@ -4,7 +4,12 @@ class Game
   def initialize(p1name, p2name)
     @player_1 = Player.new(p1name)
     @player_2 = Player.new(p2name)
+    # @players = [@player_1, @player_2]
   end
+
+  # def switch_players
+  #   @players << @players.shift
+  # end
 
   def attack(attacker, attackee)
     attackee.gets_attacked
