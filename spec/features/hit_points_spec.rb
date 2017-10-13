994f68bc0b3e7c9aa3f66d05f31a4f7ec5acd856
Chris <cdunham1989@gmail.com>
2017-10-13 14:27:32 +0100

    now shows own hit points

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -3,4 +3,9 @@ feature 'Hit points' do
     sign_in_and_play
     expect(page).to have_content 'Chris: 60HP'
   end
+
+  scenario 'see Player 1 hit points' do
+    sign_in_and_play
+    expect(page).to have_content 'Rory: 60HP'
+  end
 end
